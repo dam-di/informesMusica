9626a6b713f3452e4c6d9199fbdf257c4ed7c7ae
user-dam <dam@dam.es>
2025-05-14 17:22:53 +0200

    Proyecto

---
+++ src/main/java/dam/informesmusica/services/ReportsService.java
@@ -0,0 +1,35 @@
+package dam.informesmusica.services;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+@Service
+public class ReportsService {
+    @Autowired
+    private DataSource dataSource;
+
+    public byte[] generarReport(String reportName, Map<String, Object> parms) throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            InputStream reportStream = getClass().getClassLoader().getResourceAsStream("reports/" + reportName + ".jasper");
+
+            if (reportStream == null) {
+                throw new JRException("El informe no se encontró: " + reportName);
+            }
+            JasperPrint jasperPrint = JasperFillManager.fillReport(reportStream, parms, connection);
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        }catch (SQLException e) {
+            System.err.println("Error al obtener conexión desde el DataSource");
+            throw e;
+        }
+    }
+}
---
+++ src/test/java/dam/informesmusica/InformesMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dam.informesmusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformesMusicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
